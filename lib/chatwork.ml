5df2deba4639d2527a7eeb1268e355ac885534ae
lmdexpr <lmdexpr@gmail.com>
2022-10-14 19:55:19 +0900

    refactor and format

--- lib/chatwork.ml
+++ lib/chatwork.ml
@@ -8,16 +8,22 @@ module Me = struct
 
   let get token net =
     let request = "GET " ^ path ^ " HTTP/1.1" in
-    let headers = [ "Accept: application/json"; "x-chatworktoken: " ^ token; ] in
+
+    let headers = [ "Accept: application/json"; "x-chatworktoken: " ^ token ] in
     let headers = String.concat "\r\n" headers in
+
     let body = "" in
-    Net.with_tcp_connect ~host ~service net @@ fun conn ->
-      let req = String.concat "\r\n" [ request; headers; body ] in
+
+    let request = [ request; headers; ""; body ] in
+    let request = String.concat "\r\n" request in
+
+    let f conn =
       let buf = Buffer.create 1000 in
       Flow.(
-        copy_string req conn;
+        copy_string request conn;
         shutdown conn `Send;
-        copy conn @@ buffer_sink buf
-      );
+        copy conn @@ buffer_sink buf);
       Buffer.contents buf
+    in
+    Net.with_tcp_connect ~host ~service net f
 end
